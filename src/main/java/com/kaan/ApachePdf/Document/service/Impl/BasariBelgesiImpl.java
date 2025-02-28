ab39003c200210c66a653f0435af05a2574f9c62
KAAN\kaank <kaankhrmn.61@gmail.com>
2025-02-28 09:52:46 +0300

    Qr added

--- src/main/java/com/kaan/ApachePdf/Document/service/Impl/BasariBelgesiImpl.java
+++ src/main/java/com/kaan/ApachePdf/Document/service/Impl/BasariBelgesiImpl.java
@@ -1,5 +1,10 @@
 package com.kaan.ApachePdf.Document.service.Impl;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.kaan.ApachePdf.Document.model.CreatePdf;
 import com.kaan.ApachePdf.Document.model.PdfPattern;
 import com.kaan.ApachePdf.Document.service.BasariBelgesiService;
@@ -7,20 +12,30 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
 
 @Service
 public class BasariBelgesiImpl implements PdfPattern, BasariBelgesiService {
+
+
+    String belge_sahibi = "Kaan Kahraman";
+    String belge_tarihi = new Date().toString();
+    String belgeBilgisi = "Bu belge " + belge_tarihi + " ,  " + belge_sahibi + " tarafindan olusturulmustur.";
+
 
     @Override
     public ResponseEntity<byte[]> generateDocument(String type, String ad_soyad, String tarih) {
@@ -43,7 +58,7 @@ public class BasariBelgesiImpl implements PdfPattern, BasariBelgesiService {
 
             PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true);
             File fontFile = new File("src/main/resources/templates/arial.ttf");
-            PDType0Font font = PDType0Font.load(document,fontFile);
+            PDType0Font font = PDType0Font.load(document, fontFile);
             contentStream.setFont(font, 40);
             contentStream.setNonStrokingColor(Color.BLACK);
 
@@ -58,6 +73,13 @@ public class BasariBelgesiImpl implements PdfPattern, BasariBelgesiService {
             contentStream.endText();
 
             contentStream.close();
+
+            //QR KOD OLUŞTUR VE PDF'YE EKLE
+            PDImageXObject qrImage = createQrCodeImage(document, belgeBilgisi, 100, 100); // 100x100 px QR kod
+            PDPageContentStream qrContentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true);
+            qrContentStream.drawImage(qrImage, 70, 70, 100, 100); // Sol alt köşeye yerleştir
+            qrContentStream.close();
+
 
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             document.save(baos);
@@ -76,6 +98,18 @@ public class BasariBelgesiImpl implements PdfPattern, BasariBelgesiService {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    private static PDImageXObject createQrCodeImage(PDDocument document, String data, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+        BufferedImage image = MatrixToImageWriter.toBufferedImage(bitMatrix);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "PNG", byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+        return PDImageXObject.createFromByteArray(document, byteArray, "QRCode");
+    }
+
 
     @Override
     public ResponseEntity<byte[]> generateKisiKarti(String ad_soyad, String adres, String dogumYeri, String tckn, String cinsiyet) {
--- src/main/java/com/kaan/ApachePdf/Document/service/Impl/KisiKartiImpl.java
+++ src/main/java/com/kaan/ApachePdf/Document/service/Impl/KisiKartiImpl.java
@@ -1,5 +1,10 @@
 package com.kaan.ApachePdf.Document.service.Impl;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.kaan.ApachePdf.Document.model.CreatePdf;
 import com.kaan.ApachePdf.Document.model.PdfPattern;
 import com.kaan.ApachePdf.Document.service.KisiKartiService;
@@ -8,20 +13,28 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
 
 @Service
 public class KisiKartiImpl implements PdfPattern, KisiKartiService {
+
+    String belge_sahibi = "Kaan Kahraman";
+    String belge_tarihi = new Date().toString();
+    String belgeBilgisi = "Bu belge " + belge_tarihi + " ,  " + belge_sahibi + " tarafindan olusturulmustur.";
 
     @Override
     public ResponseEntity<byte[]> generateDocument(String type, String ad_soyad, String adres, String dogumYeri, String tckn, String cinsiyet) {
@@ -75,6 +88,12 @@ public class KisiKartiImpl implements PdfPattern, KisiKartiService {
             contentStream.showText(adres);
             contentStream.endText();
 
+            //QR KOD OLUŞTUR VE PDF'YE EKLE
+            PDImageXObject qrImage = createQrCodeImage(document, belgeBilgisi, 100, 100); // 100x100 px QR kod
+            PDPageContentStream qrContentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true);
+            qrContentStream.drawImage(qrImage, 70, 70, 100, 100); // Sol alt köşeye yerleştir
+            qrContentStream.close();
+
             contentStream.close();
 
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -94,6 +113,17 @@ public class KisiKartiImpl implements PdfPattern, KisiKartiService {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    private static PDImageXObject createQrCodeImage(PDDocument document, String data, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+        BufferedImage image = MatrixToImageWriter.toBufferedImage(bitMatrix);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "PNG", byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+        return PDImageXObject.createFromByteArray(document, byteArray, "QRCode");
     }
 
 
